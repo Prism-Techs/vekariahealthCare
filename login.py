8b9bc1c59c2a5587133d8b14fd07940ed968f1f4
ved shah <veds2103@gmail.com>
2024-11-25 16:21:04 +0530

    server

--- login.py
+++ login.py
@@ -18,7 +18,7 @@ class Ui_Form(object):
         Form.setObjectName("Form")
         Form.resize(1024, 600)
         Form.setStyleSheet("background-color:black;")
-        Form.setWindowFlags(Qt.FramelessWindowHint |  Qt.WindowStaysOnBottomHint)
+        Form.setWindowFlags(Qt.WindowStaysOnBottomHint)
         # Form.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         self.wifi_window = None  # Initialize as None
         # Initialize keyboard tracking variables
