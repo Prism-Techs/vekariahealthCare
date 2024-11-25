1ba434287265d28faf81865b45dfa79dd478ed83
ved shah <veds2103@gmail.com>
2024-11-25 15:32:21 +0530

    doctor page

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 from home import Ui_Form as home_page
-from pai import Ui_Form as patient_page
+from pai import Patient_info as patient_page
 from login import Login_page
--- doctor.py
+++ doctor.py
@@ -15,6 +15,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtCore import Qt, QRect, QTimer, QDateTime
 from . import patient_page as Patient_info_page
+# from pai imprt 
 
 class HomePpage(object):
     def setupUi(self, Form):
