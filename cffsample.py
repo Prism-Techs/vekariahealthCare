36029e128c4a25adf9333fdc5be4f044f4aeca96
ved shah <veds2103@gmail.com>
2024-12-03 20:02:54 +0530

    flciker added

--- cffsample.py
+++ cffsample.py
@@ -16,19 +16,18 @@ class CFFTest(QMainWindow):
         self.response_array = [0,0,0,0,0]
         self.min_apr = 0
         self.max_apr = 0
-        globaladc.flicker_Prepair()
-
-
+        self.flicker_bool = True
         
-        # Initialize global ADC
+        globaladc.flicker_Prepair()
         self.globaladc = globaladc
+        self.globaladc.fliker_start_g()
         
         self.setupUI()
         self.setupGPIO()
         
         self.timer = QTimer()
         self.timer.timeout.connect(self.periodic_event)
-        self.timer.start(1000) # Adjust interval as needed
+        self.timer.start(1000)
 
     def setupUI(self):
         self.setWindowTitle('CFF Test')
@@ -38,7 +37,6 @@ class CFFTest(QMainWindow):
         self.setCentralWidget(centralWidget)
         layout = QGridLayout()
         
-        # Labels
         self.cff_label = QLabel('CFF FOVEA:', self)
         self.cff_label.setStyleSheet("font: 15pt Arial")
         
@@ -54,12 +52,10 @@ class CFFTest(QMainWindow):
         self.action_label = QLabel("Patient's side Button\nBegins Trial", self)
         self.action_label.setStyleSheet("font: 15pt Arial; background-color: white")
         
-        # Trial List
         self.trial_list = QListWidget()
         self.trial_list.setStyleSheet("font: 15pt Arial")
         self.trial_list.setMaximumWidth(100)
         
-        # Add widgets to layout
         layout.addWidget(self.cff_label, 0, 1)
         layout.addWidget(self.min_label, 1, 1)
         layout.addWidget(self.max_label, 1, 2)
@@ -87,35 +83,33 @@ class CFFTest(QMainWindow):
             self.response_array[self.response_count] = self.freq_val
             self.trial_list.addItem(str(self.freq_val))
             
-            # Calculate min value
             self.min_apr = self.globaladc.get_cff_f_min_cal(self.response_count, self.freq_val)
             self.min_label.setText(str(self.min_apr))
             
             self.response_count += 1
             
             if self.response_count >= 5:
-                # Calculate max value
                 self.max_apr = self.globaladc.get_cff_f_max_cal()
                 self.max_label.setText(str(self.max_apr))
                 
-                # Calculate average
                 avg_val = self.globaladc.get_cff_f_avg_cal()
                 self.globaladc.get_print(f"Average value: {avg_val}")
                 
-                # Final buzzer
                 self.globaladc.buzzer_3()
                 time.sleep(1)
                 
                 self.timer.stop()
                 self.close()
-            globaladc.fliker_start_g()
-            self.globaladc.buzzer_3()
 
     def periodic_event(self):
         if not self.skip_event:
             self.freq_val = round(self.freq_val - 0.5, 1)
             self.freq_label.setText(str(self.freq_val))
             self.globaladc.put_cff_fovea_frq(self.freq_val)
+            
+            # Toggle flicker state
+            self.globaladc.fliker(1 if self.flicker_bool else 0)
+            self.flicker_bool = not self.flicker_bool
             
             if self.freq_val < 5:
                 self.skip_event = True
@@ -124,6 +118,7 @@ class CFFTest(QMainWindow):
                 self.globaladc.buzzer_3()
 
     def closeEvent(self, event):
+        self.globaladc.fliker(0)
         GPIO.cleanup()
         event.accept()
 
