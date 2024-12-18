ad99f4dbbc4b02aba3f1fc4f3dc71ef024199c79
ved shah <veds5861@gmail.com>
2024-12-18 19:41:03 +0530

    s

--- UI/Keyboard.py
+++ UI/Keyboard.py
@@ -33,12 +33,13 @@ class KeyBoard:
     def createAlphaKey(self, root, entry, number=False):
 
         alphabets = [
-            ['@','#','/'],
-            [ 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+            ['1','2','3','4','5','6','7','8','9','0','/','@'],
+            ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
             [ 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Back'],
             [ 'Z', 'X', 'C', 'V', 'B', 'N', 'M','Enter'],
             ['Space']
         ]
+        
         
         window = tk.Toplevel(root)
         x = root.winfo_x()
