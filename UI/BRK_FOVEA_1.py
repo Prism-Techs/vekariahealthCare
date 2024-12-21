57642814a9bd271f963e903aa1263c7dde4cf9c7
ved shah <veds5861@gmail.com>
2024-12-21 13:49:15 +0530

    s

--- UI/BRK_FOVEA_1.py
+++ UI/BRK_FOVEA_1.py
@@ -318,7 +318,7 @@ class BrkFovea_1 :
                                    bg='black', fg='white',
                                    textvariable=str(self.depthVal),
                                    relief='solid', borderwidth=2)
-        self.DepthVal.place(x=120,y=80)
+        self.DepthVal.place(x=150,y=50)
         self.UPButton.place (x=110,  y=75)   
         self.DownButton.place (x=110,  y=200)
         self.saveButton.place_forget()
